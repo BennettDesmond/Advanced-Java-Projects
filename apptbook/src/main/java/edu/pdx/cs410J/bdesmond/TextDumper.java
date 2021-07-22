466cf894a41f663fdd4cf485da3d5663cc9c43a9
Bennett Desmond <bennett.a.desmond@gmail.com>
2021-07-21 18:05:55 -0700

    Deleted Bogus Classes

--- apptbook/src/main/java/edu/pdx/cs410J/bdesmond/TextDumper.java
+++
@@ -1,6 +0,0 @@
-package edu.pdx.cs410J.bdesmond;
-
-import edu.pdx.cs410J.AppointmentBookDumper;
-
-public class TextDumper {
-}
--- apptbook/src/main/java/edu/pdx/cs410J/bdesmond/TextParser.java
+++
@@ -1,6 +0,0 @@
-package edu.pdx.cs410J.bdesmond;
-
-import edu.pdx.cs410J.AppointmentBookParser;
-
-public class TextParser {
-}
