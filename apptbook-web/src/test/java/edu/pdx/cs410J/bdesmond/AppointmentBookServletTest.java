c3ede9a94b9af636bdfe34ab669735a9dff0d1b4
Bennett Desmond <bennett.a.desmond@gmail.com>
2021-07-24 16:52:45 -0700

    Edited the Html page and Adjusted the servlet tests

--- apptbook-web/src/test/java/edu/pdx/cs410J/bdesmond/AppointmentBookServletTest.java
+++ apptbook-web/src/test/java/edu/pdx/cs410J/bdesmond/AppointmentBookServletTest.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.Collection;
+import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -30,22 +31,27 @@ public class AppointmentBookServletTest {
     AppointmentBook book = servlet.createAppointmentBook(owner);
     book.addAppointment(new Appointment(description));
 
+    Map<String,String> queryParams = Map.of("owner",owner);
+    StringWriter sw = invokeServletMethod(queryParams,servlet::doGet);
+
+    String text = sw.toString();
+    assertThat(text, containsString(owner));
+    assertThat(text, containsString(description));
+  }
+
+  private StringWriter invokeServletMethod(Map<String,String> params, ServletMethodInvoker invoker) throws IOException, ServletException {
     HttpServletRequest request = mock(HttpServletRequest.class);
-    when(request.getParameter("owner")).thenReturn(owner);
+    params.forEach((key,value) -> when(request.getParameter(key)).thenReturn(value));
+
     HttpServletResponse response = mock(HttpServletResponse.class);
 
     StringWriter sw = new StringWriter();
     when(response.getWriter()).thenReturn(new PrintWriter(sw));
 
-    servlet.doGet(request, response);
+    invoker.invoke(request,response);
 
-    //int expectedWords = 0;
-    //verify(new PrintWriter(sw)).println(Messages.formatWordCount(expectedWords));
     verify(response).setStatus(HttpServletResponse.SC_OK);
-
-    String text = sw.toString();
-    assertThat(text, containsString(owner));
-    assertThat(text, containsString(description));
+    return sw;
   }
 
   @Test
@@ -55,27 +61,7 @@ public class AppointmentBookServletTest {
     String owner = "John";
     String description = "Teach Java";
 
-    HttpServletRequest request = mock(HttpServletRequest.class);
-    when(request.getParameter("owner")).thenReturn(owner);
-    when(request.getParameter("description")).thenReturn(description);
-
-    HttpServletResponse response = mock(HttpServletResponse.class);
-
-    // Use a StringWriter to gather the text from multiple calls to println()
-    //StringWriter stringWriter = new StringWriter();
-    //PrintWriter pw = new PrintWriter(stringWriter, true);
-
-    //when(response.getWriter()).thenReturn(pw);
-
-    servlet.doPost(request, response);
-
-    //assertThat(stringWriter.toString(), containsString(Messages.definedWordAs(word, definition)));
-
-    // Use an ArgumentCaptor when you want to make multiple assertions against the value passed to the mock
-    ArgumentCaptor<Integer> statusCode = ArgumentCaptor.forClass(Integer.class);
-    verify(response).setStatus(statusCode.capture());
-
-    assertThat(statusCode.getValue(), equalTo(HttpServletResponse.SC_OK));
+    invokeServletMethod(Map.of("owner",owner,"description",description), servlet::doPost);
 
     AppointmentBook book = servlet.getAppointmentBook(owner);
     assertThat(book, notNullValue());
@@ -87,7 +73,10 @@ public class AppointmentBookServletTest {
     Appointment appointment = appointments.iterator().next();
     assertThat(appointment.getDescription(), equalTo(description));
 
-    //assertThat(servlet.getDefinition(word), equalTo(definition));
+  }
+
+  private interface ServletMethodInvoker {
+    void invoke(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
   }
 
 }
